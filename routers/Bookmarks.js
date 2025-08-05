137ced473bffbb8c99d9a48bdee78c4ecd5fa0fd
stark302 <parthibanprince143@gmail.com>
2025-08-05 14:38:57 +0530

    add BookmarkAuth

--- routers/Bookmarks.js
+++ routers/Bookmarks.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import {addBookmark ,removeBookmark ,Bookmarks} from '../controller/BookmarkController.js'
+import Authenication from '../utils/Authenication.js'
 
 const router  = express.Router()
 
-router.post('/addBookmark',addBookmark, (req,res)=>{
+router.post('/addBookmark',Authenication,addBookmark, (req,res)=>{
     console.log(req)
     res.json("I am getting")
 })
 
-router.get('/userBookmark',Bookmarks)
+router.get('/userBookmark',Authenication,Bookmarks)
 
-router.delete('/removeBookmark',removeBookmark)
+router.delete('/removeBookmark',Authenication,removeBookmark)
 
 export default router
